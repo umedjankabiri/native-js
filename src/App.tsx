3cabd9f930b35b44d19592cf6f632b3987faa272
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-14 00:36:17 +0300

    Demonstrate usage of map method

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <div><h1>Native JavaScript</h1></div>
-    </>
-  )
+    const names = ['Dimych', 'Sveta', 'Katya', 'Viktor', 'Ignat'];
+    const nameList = names.map((name, index) => (
+        <li key={index}>{name}</li>
+    ));
+    const users = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Sveta"},
+        {id: 3, name: "Katya"},
+        {id: 4, name: "Viktor"},
+        {id: 5, name: "Ignat"},
+    ]
+    const userList = users.map(user => (
+        <li key={user.id}>{user.name}</li>
+    ));
+    return (
+        <>
+            <div><h1>Native JavaScript</h1></div>
+            <ul>
+                {nameList}
+            </ul>
+            <ul>
+                {userList}
+            </ul>
+        </>
+    )
 }
 
 export default App
