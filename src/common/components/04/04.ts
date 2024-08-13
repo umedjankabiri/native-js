fb11f189408b881ff1bcc2f210cc0b58612cea8d
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-13 22:52:46 +0300

    An array of "tasks", an array of "courses", and a "chipPredicate" function have been added to test them

--- src/common/components/04/04.ts
+++ src/common/components/04/04.ts
@@ -1,2 +1,17 @@
+import {CourseProps} from "common/types/CourseProps.ts";
+
 export const ages = [18, 20, 22, 1, 100, 90, 14];
 export const predicate = (age: number) => age > 90;
+
+export const courses = [
+    {title: "CSS", price: 110},
+    {title: "JS", price: 200},
+    {title: "REACT", price: 150}
+]
+export const chipPredicate = (course: CourseProps) => course.price < 160
+export const tasks = [
+    {id: 1, title: "Bread", isDone: false},
+    {id: 2, title: "Milk", isDone: true},
+    {id: 3, title: "Sold", isDone: false},
+    {id: 4, title: "Sugar", isDone: true},
+]
