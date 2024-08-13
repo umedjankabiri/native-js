8231552bcf6a5ec0f5d11469b5f76118734f08c7
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-14 01:39:10 +0300

    created a test, description of the test: "The props object should be destructured in the component"

--- src/common/components/06/06.test.ts
+++ src/common/components/06/06.test.ts
@@ -13,3 +13,19 @@ beforeEach(() => {
         }
     }
 })
+
+test("The props object should be destructured in the component", () => {
+    // const age = props.age
+    // const lessons = props.lessons
+    const a = props.age
+    const l = props.lessons
+
+    const {age, lessons} = props
+    const {title} = props.address.street
+
+    expect(age).toBe(32);
+    expect(lessons.length).toBe(3);
+    expect(a).toBe(32);
+    expect(l.length).toBe(3);
+    expect(title).toBe('Selim sk.');
+})
