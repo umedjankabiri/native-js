39ac6238943dd9a7f0cda03ae495ec1be55a2529
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-13 20:19:06 +0300

    created a test, description of the test: "The city test should contain 3 houses."

--- src/common/components/02/02.test.ts
+++ src/common/components/02/02.test.ts
@@ -62,3 +62,22 @@ beforeEach(() => {
         citizensNumber: 1000000
     }
 })
+
+test("The 'city' test should contain 3 houses.", ()=> {
+    expect(city.houses.length).toBe(3);
+
+    expect(city.houses[0].buildedAt).toBe(2012);
+    expect(city.houses[0].repaired).toBe(false);
+    expect(city.houses[0].address.street.title).toBe("White street");
+    expect(city.houses[0].address.number).toBe(100);
+
+    expect(city.houses[1].buildedAt).toBe(2008);
+    expect(city.houses[1].repaired).toBe(false);
+    expect(city.houses[1].address.street.title).toBe("Happy street");
+    expect(city.houses[1].address.number).toBe(100);
+
+    expect(city.houses[2].buildedAt).toBe(2020);
+    expect(city.houses[2].repaired).toBe(false);
+    expect(city.houses[2].address.street.title).toBe("Happy street");
+    expect(city.houses[2].address.number).toBe(101);
+})
