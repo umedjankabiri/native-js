52f8cf1d22697ca253db32c4596c25685c6238dd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-13 23:27:21 +0300

    added a "getStreetsTitlesOfGovernmentsBuildings" function for testing

--- src/common/components/04/04_01.ts
+++ src/common/components/04/04_01.ts
@@ -1,4 +1,6 @@
-import {CityProps} from "common/types/TestsProps.ts";
+import {CityProps, GovernmentBuildingsProps} from "common/types/TestsProps.ts";
 
 export const demolishHousesOnTheStreet = (city: CityProps, street: string) =>
     city.houses = city.houses.filter(house => house.address.street.title !== street)
+export const getStreetsTitlesOfGovernmentsBuildings = (buildings: GovernmentBuildingsProps[]) =>
+    buildings.map(street => street.address.street)
