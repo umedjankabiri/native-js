6349d12b786424f0861de2cedea4ac41469ed4ae
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-13 20:09:14 +0300

    A 02.test.ts file has been created for tests. A 'city' object was created, and its initialization was added inside the beforeEach() function.

---
+++ src/common/components/02/02.test.ts
@@ -0,0 +1,64 @@
+import {CityProps} from "common/types/TestsProps.ts";
+
+let city: CityProps;
+
+beforeEach(() => {
+    city = {
+        title: "New York",
+        houses: [
+            {
+                buildedAt: 2012,
+                repaired: false,
+                address: {
+                    number: 100,
+                    street: {
+                        title: "White street"
+                    }
+                }
+            },
+            {
+                buildedAt: 2008,
+                repaired: false,
+                address: {
+                    number: 100,
+                    street: {
+                        title: ("Happy street")
+                    }
+                }
+            },
+            {
+                buildedAt: 2020,
+                repaired: false,
+                address: {
+                    number: 101,
+                    street: {
+                        title: ("Happy street")
+                    }
+                }
+            }
+        ],
+        governmentBuildings: [
+            {
+                type: "HOSPITAL",
+                budget: 200000,
+                staffCount: 2000,
+                address: {
+                    street: {
+                        title: "Central Str"
+                    }
+                }
+            },
+            {
+                type: "FIRE-STATION",
+                budget: 50000,
+                staffCount: 200,
+                address: {
+                    street: {
+                        title: "South Str"
+                    }
+                }
+            },
+        ],
+        citizensNumber: 1000000
+    }
+})
