de3008ae6251971ec943163e728bacefd2241190
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-14 00:16:16 +0300

    added a "createMessages" function for testing

--- src/common/components/05/05_01.ts
+++ src/common/components/05/05_01.ts
@@ -4,3 +4,5 @@ export const getStreetsTitlesOfGovernmentsBuildings = (buildings: GovernmentBuil
     buildings.map(street => street.address.street.title)
 export const getStreetsTitlesOfHouses = (houses: HousesProps[]) =>
     houses.map(house => house.address.street.title);
+export const createMessages = (houses: HousesProps[])=>
+    houses.map(house => `Hello guys from ${house.address.street.title}`)
