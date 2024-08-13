ac3eb207a912de597c76330bac606ddabec3b7b0
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-14 01:46:17 +0300

    created a test, description of the test: "The array should be destructured"

--- src/common/components/06/06.test.ts
+++ src/common/components/06/06.test.ts
@@ -29,3 +29,25 @@ test("The props object should be destructured in the component", () => {
     expect(l.length).toBe(3);
     expect(title).toBe('Selim sk.');
 })
+test("The array should be destructured", () => {
+    const l1 = props.lessons[0]
+    const l2 = props.lessons[1]
+    // const [, lsn2] = props.lessons
+    const [lsn1, ...restLessons1] = props.lessons
+    const [, lsn2, ...restLessons2] = props.lessons
+    const [ls1, ls2] = props.lessons
+
+    expect(l1.title).toBe('1')
+    expect(l2.title).toBe('2')
+    expect(ls1.title).toBe('1')
+    expect(ls2.title).toBe('2')
+    expect(lsn1.title).toBe('1')
+    expect(lsn2.title).toBe('2')
+    expect(restLessons1.length).toBe(2)
+    expect(restLessons1[0].title).toBe('2')
+    expect(restLessons1[0]).toStrictEqual({title: '2'})
+    expect(restLessons1[1]).toStrictEqual({title: '3', name: "react"})
+    expect(restLessons1[1]).toStrictEqual({name: "react", title: '3'}) // key and value was swapped
+    expect(restLessons2.length).toBe(1)
+    expect(restLessons2[0].title).toBe('3')
+})
