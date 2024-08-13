6ec5ab59c7c0d38506df246e82bb684174252e11
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-14 00:18:31 +0300

    A 05_01.test.ts file has been created for tests. A 'city' object was created, and its initialization was added inside the beforeEach() function

---
+++ src/common/components/05/05_01.test.ts
@@ -0,0 +1,67 @@
+import {CityProps} from "common/types/TestsProps.ts";
+
+let city: CityProps
+
+beforeEach(() => {
+    city = {
+        title: "New York",
+        houses: [
+            {
+                id: 1,
+                buildedAt: 2012,
+                repaired: false,
+                address: {
+                    number: 100,
+                    street: {
+                        title: "White street"
+                    }
+                }
+            },
+            {
+                id: 2,
+                buildedAt: 2008,
+                repaired: false,
+                address: {
+                    number: 100,
+                    street: {
+                        title: ("Happy street")
+                    }
+                }
+            },
+            {
+                id: 3,
+                buildedAt: 2020,
+                repaired: false,
+                address: {
+                    number: 101,
+                    street: {
+                        title: ("Happy street")
+                    }
+                }
+            }
+        ],
+        governmentBuildings: [
+            {
+                type: "HOSPITAL",
+                budget: 200000,
+                staffCount: 2000,
+                address: {
+                    street: {
+                        title: "Central Str"
+                    }
+                }
+            },
+            {
+                type: "FIRE-STATION",
+                budget: 50000,
+                staffCount: 200,
+                address: {
+                    street: {
+                        title: "South Str"
+                    }
+                }
+            },
+        ],
+        citizensNumber: 1000000
+    }
+})
