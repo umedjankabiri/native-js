f5375264c3cae23a36bfad62b04a0807b98a4fea
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-13 16:38:45 +0300

    created a test, Description of the test: "The multiply function should return the correct value"

--- src/common/components/01/01.test.ts
+++ src/common/components/01/01.test.ts
@@ -1,9 +1,25 @@
-import {addition} from "common/components/01/01.ts";
+import {addition, multiply} from "common/components/01/01.ts";
+
+let a: number;
+let b: number;
+
+beforeEach(()=> {
+    a = 10;
+    b = 10;
+})
 
 test("addition should be correct value", () => {
-    const a: number = 10;
-    const b: number = 10;
     const result = addition(a, b);
 
     expect(result).toBe(20);
+})
+test("multiply should be correct value", () => {
+    let result1 = multiply(a, b);
+
+    expect(result1).toBe(100);
+
+    b = 2;
+    let result2 = multiply(a, b);
+
+    expect(result2).toBe(20);
 })
