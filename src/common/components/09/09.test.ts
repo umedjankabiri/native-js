52d7044d2ed82f911972138b36b19d483686339a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-27 17:21:53 +0300

    Rename test and check immutability principles
    Renamed the test to better reflect its purpose and added an assertion to ensure that the address object remains immutable. This helps in verifying that the original client object is not altered unintentionally.

--- src/common/components/09/09.test.ts
+++ src/common/components/09/09.test.ts
@@ -1,15 +1,18 @@
 import {ClientProps} from "common/types/ClientProps.ts";
 import {makeHairstyle} from "common/components/09/09.ts";
 
-test("", ()=> {
+test("immutability principle of object", () => {
     let client: ClientProps = {
         name: "Dmitry",
         hair: 32,
-        address: {title: "Minsk"}
+        address: {
+            title: "Minsk"
+        }
     }
 
     const HairstylesClient = makeHairstyle(client, 2)
 
     expect(client.hair).toBe(32)
     expect(HairstylesClient.hair).toBe(16)
+    expect(HairstylesClient.address).toBe(client.address)
 })
