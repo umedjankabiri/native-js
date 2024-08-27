625f872c488ef783b99e9f06cf69e53c311975bd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-27 17:05:13 +0300

    Refactor makeHairstyle to return modified client directly
    Previously, a copy of the client object was created and modified before being returned. Now, the function directly returns the modified client object, reducing redundancy and improving readability.

--- src/common/components/09/09.ts
+++ src/common/components/09/09.ts
@@ -1,8 +1,8 @@
 import {ClientProps} from "common/types/ClientProps.ts";
 
-export const makeHairstyle = (client: ClientProps, power: number)=> {
-    const copyClient = {...client}
-    copyClient.hair /= power
-
-    return copyClient
+export const makeHairstyle = (client: ClientProps, power: number) => {
+    return {
+        ...client,
+        hair: client.hair / power
+    }
 }
