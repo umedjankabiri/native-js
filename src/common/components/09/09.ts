8f41531df06eb7379a3ced8ba882abc336a65314
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-30 18:36:49 +0300

    Add address and laptop update functions for client
    Implemented `changeClientAddress` to update the client's city and `changeLaptop` to update the client's laptop title. This enhances client data manipulation functionality.

--- src/common/components/09/09.ts
+++ src/common/components/09/09.ts
@@ -1,8 +1,33 @@
-import {ClientProps} from "common/types/ClientProps.ts";
+import {ClientProps, ClientWithLaptopProps} from "common/types/ClientProps.ts";
 
+export const client: ClientProps = {
+    name: "Dmitry",
+    hair: 32,
+    address: {
+        city: "Minsk"
+    }
+}
 export const makeHairstyle = (client: ClientProps, power: number) => {
     return {
         ...client,
         hair: client.hair / power
+    }
+}
+export const changeClientAddress = (client: ClientProps, city: string)=> {
+    return {
+        ...client,
+        address: {
+            ...client.address,
+            city: city
+        }
+    }
+}
+export const changeLaptop = (client: ClientWithLaptopProps, laptop: string) => {
+    return {
+        ...client,
+        laptop: {
+            ...client.laptop,
+            title: laptop
+        }
     }
 }
